c73effb07468a0edba6a8191f63db30788ff7c90
Vanderson Gabryel <139379635+VandeGabryel@users.noreply.github.com>
2023-07-14 13:24:57 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,31 @@
+const personagens = document.querySelectorAll('.personagem')
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+
+        if(window.innerWidth < 450) {
+            window.scrollTo({top: 0, behavior: 'smooth' });
+        }
+
+        const personagemSelecionado = document.querySelector('.selecionado');
+        personagemSelecionado.classList.remove('selecionado')
+
+        personagem.classList.add('selecionado');
+
+        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+
+        const idPersonagem = personagem.attributes.id.value;
+        imagemPersonagemGrande.src = `./src/imagens/png/${idPersonagem}.png`;
+
+
+        const nomePersonagem = document.getElementById('nome-personagem');
+        nomePersonagem.innerText = personagem.getAttribute('data-name');
+
+        const descricaoPersonagem = document.getElementById('descricao-personagem');
+        descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+
+        
+    })
+})
+
+alert("Para uma melhor expêriencia, veja em modo tela cheia (F11)")
